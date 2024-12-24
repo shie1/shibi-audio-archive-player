80ab2c6c39c9615cec6f40da6f3a48e1ad9494b3
Sonkoly Bence <sonkolyw@gmail.com>
2024-12-24 22:34:58 +0100

    added mantine

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,28 @@
-import { MusicPlayer, useMusicPlayer } from "@/components/musicPlayer";
+import '@mantine/core/styles.css';
 import "@/styles/globals.css";
+
+import { MusicPlayer, useMusicPlayer } from "@/components/musicPlayer";
 import type { AppProps } from "next/app";
 import { createContext, useRef } from "react";
+import { Button, createTheme, MantineProvider } from '@mantine/core';
 
 export const MusicPlayerContext = createContext<null | MusicPlayer>(null);
+
+const theme = createTheme({})
 
 export default function App({ Component, pageProps }: AppProps) {
   const audioRef = useRef<HTMLAudioElement | null>(null);
   const musicPlayer = useMusicPlayer(audioRef);
 
   return <>
-    <audio ref={audioRef} />
-    <MusicPlayerContext.Provider value={musicPlayer}>
-      <Component {...pageProps} />
-      <button onClick={()=>{
-        musicPlayer.playSong(`${process.env.API}/mosopor/tovabb/01_tovabb`)
-      }}>play</button>
-    </MusicPlayerContext.Provider>
+    <MantineProvider theme={theme} defaultColorScheme='dark'>
+      <audio ref={audioRef} />
+      <MusicPlayerContext.Provider value={musicPlayer}>
+        <Component {...pageProps} />
+        <Button onClick={() => {
+          musicPlayer.playSong(`${process.env.API}/mosopor/tovabb/01_tovabb`)
+        }}>play</Button>
+      </MusicPlayerContext.Provider>
+    </MantineProvider>
   </>;
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { ColorSchemeScript, mantineHtmlProps } from '@mantine/core';
 
 export default function Document() {
   return (
-    <Html lang="hu">
-      <Head />
+    <Html lang="hu" {...mantineHtmlProps}>
+      <Head>
+        <ColorSchemeScript defaultColorScheme="auto" />
+      </Head>
       <body>
         <Main />
         <NextScript />
